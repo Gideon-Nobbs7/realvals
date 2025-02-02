c119919c64c19618ec31227aebf593332baaa205
Giiid <gideonfummey1@gmail.com>
2025-02-02 20:02:29 +0000

    new update

--- vals/models.py
+++ vals/models.py
@@ -11,3 +11,6 @@ class Customer(models.Model):
     email = models.EmailField(default="myval@gmail.com")
     phone_number = models.CharField(max_length=20, default="0247965219")
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
